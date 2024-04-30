6f9040295de3047b49e8e7c150305ae06ba64115
shamilvasiyev <vasiyevsamil@gmail.com>
2024-05-01 01:59:48 +0400

    bug fixed on bootstrap styling

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,27 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <PageContainer>
+        <div className="container">
+          <HomeGalery />
+        </div>
+
+        <div>
+          <Container>
+            <Row>
+              <Col className="mt-5" xs={12} sm={6} lg={6} xl={6}>
+                <Homeimg img="./header/photo11.png" />
+              </Col>
+
+              <Col className="mt-5" xs={12} sm={6} lg={6} xl={6}>
+                <Homeimg img="./header/photo11.png" />
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </PageContainer>
+    </>
+  );
 };
 
 export default Home;
