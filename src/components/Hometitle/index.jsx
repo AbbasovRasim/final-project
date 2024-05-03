9da3d6c8d241cb1e191c0411143ce261c9c1f571
shamilvasiyev <vasiyevsamil@gmail.com>
2024-05-04 01:41:37 +0400

    server is up...

---
+++ src/components/Hometitle/index.jsx
@@ -0,0 +1,14 @@
+function Hometitle(props) {
+  const { title, desc } = props;
+  return (
+    <div>
+      {title}
+      {/* {title2} */}
+      {/* {title3} */}
+      {desc}
+      {/* {desc2} */}
+      {/* {desc3} */}
+    </div>
+  );
+}
+export default Hometitle;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -7,6 +7,109 @@ const Home = () => {
   return (
     <>
       <PageContainer>
+        <div className="background">
+          <div className="container ">
+            <HomeGalery />
+          </div>
+          <h1 style={{ textAlign: "center" }}>Xidmətlərimiz</h1>
+          <div>
+            <Container>
+              <Row>
+                <Col className="mt-5" xs={12} sm={6} lg={6} xl={6}>
+                  <Homeimg img="./header/photo11.png" />
+                </Col>
+                <Col className="mt-5 " xs={12} sm={6} lg={6} xl={6}>
+                  <Hometitle title="İnteryer dizayn" />
+                  <Hometitle desc="Hər bir məkanın daxili dizaynının ayrılmaz hissəsi gözəllik və funksionallıqdırsa orada istifadə olunan aksesuar, dekor, işıq sistemi isə o dizayna fərqlilik verməsi, yadda qalan etməsidir." />
+
+                  <button>Daha ətraflı</button>
+                </Col>
+              </Row>
+              <Row>
+                <Col className="mt-5" xs={12} sm={6} lg={6} xl={6}>
+                  <Hometitle title="Eksteryer və Landşaft dizayn" />
+                  <Hometitle desc="Eksteryer dedikde ilk öncə ağıla gələn layihənin fasad görünüşüdür. Fasad dizaynında mütləq olaraq bina ölçüləri nəzərə alınmalıdır. Bu ölçülərə əsasən də üslub və bu üsluba uyğun materialı düzgün seçmək lazımdır." />
+
+                  <button>Daha ətraflı</button>
+                </Col>
+                <Col className="mt-5" xs={12} sm={6} lg={6} xl={6}>
+                  <Homeimg img="./header/photo14.png" />
+                </Col>
+              </Row>
+
+              <Row>
+                <Col className="mt-5" xs={12} sm={6} lg={6} xl={6}>
+                  <Homeimg img="./header/photo15.png" />
+                </Col>
+                <Col className="mt-5 " xs={12} sm={6} lg={6} xl={6}>
+                  <Hometitle title="Ticarət Obyektlərinin dizaynı" />
+                  <Hometitle desc="Hər bir məkanın daxili dizaynının ayrılmaz hissəsi gözəllik və funksionallıqdırsa orada istifadə olunan aksesuar, dekor, işıq sistemi isə o dizayna fərqlilik verməsi, yadda qalan etməsidir." />
+
+                  <button>Daha ətraflı</button>
+                </Col>
+              </Row>
+            </Container>
+
+            <h1 style={{ textAlign: "center" }}>İşimizin mərhələləri</h1>
+            <Container>
+              <Row>
+                <Col className="mt-5" xs={12} sm={3} lg={3} xl={3}>
+                  <Homejobstep title="01" />
+                  <Homejobstep desc="Ölçü Götürülməsi" />
+                  <Homejobstep desc="Layihələrin mövcud vəziyyətinin mövcud vəziyyətinin ölçülərinin götürülməsi, qeydlərin aparılması" />
+                </Col>
+
+                <Col className="mt-5" xs={12} sm={3} lg={3} xl={3}>
+                  <Homejobstep title="02" />
+                  <Homejobstep desc="Bölgü planı" />
+                  <Homejobstep desc="Məkanın beton elementlərini eləcə də havalandırma santexnika şaxtalarının yerlərini nəzərə alaraq müştərinin də istəklərinə uyğun " />
+                </Col>
+
+                <Col className="mt-5" xs={12} sm={3} lg={3} xl={3}>
+                  <Homejobstep title="03" />
+                  <Homejobstep desc="Mebel həlli planı" />
+                  <Homejobstep desc="Bərpa planından sonra yenilənmiş layihənin otaq bölgülərinə əsasən müştərinin istəyi ilə birgə müəyyən standartları da nəzərə alaraq mebel yerləşiminin işlənməsi." />
+                </Col>
+
+                <Col className="mt-5" xs={12} sm={3} lg={3} xl={3}>
+                  <Homejobstep title="04" />
+                  <Homejobstep desc="Elektrik və santexnika təchizatı planı" />
+                  <Homejobstep desc="Mebel həlli planı hazır olduqdan sonra yerləşimə əsasən elektrik və santexnika ölçülərinin işlənib hazırlanması." />
+                </Col>
+              </Row>
+
+              <Row>
+                <Col className="mt-5" xs={12} sm={3} lg={3} xl={3}>
+                  <Homejobstep title="05" />
+                  <Homejobstep
+                    style={{ color: "red" }}
+                    desc="Məkanın dizayn və vizualizasiyası"
+                  />
+                  <Homejobstep desc="Yerləşim və mebel həlli planı hazır olduqdan sonra müştərinin də müəyyən istəkləri nəzərə alınaraq düzgün üslub seçimi ilə məkanın 3D, vizualizasiya işlərinin görülməsi." />
+                </Col>
+
+                <Col className="mt-5" xs={12} sm={3} lg={3} xl={3}>
+                  <Homejobstep title="06" />
+                  <Homejobstep desc="Tavan ölçüləri" />
+                  <Homejobstep desc="Təsdiq olunmuş dizayna əsasən tavan alçıpan və elektrik ölçü xətlərinin düzgün işlənməsi. " />
+                </Col>
+
+                <Col className="mt-5" xs={12} sm={3} lg={3} xl={3}>
+                  <Homejobstep title="07" />
+                  <Homejobstep desc="Kosmetik işlərin görülməsi" />
+                  <Homejobstep desc="Dizaynda istifadə olunmuş bütün materialların ölçülərinin çıxarılması və matrial alımı üçün bütün məlumatların hazırlanması" />
+                </Col>
+
+                <Col className="mt-5" xs={12} sm={3} lg={3} xl={3}>
+                  <Homejobstep title="08" />
+                  <Homejobstep desc="Dekor və mebel" />
+                  <Homejobstep desc="Dizaynda istifadə olunan bütün dekor və mebellərin ölçülərinin düzgün çıxarılması və müştərinin sifarişi üçün mebel dükanına göndərilməsi." />
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </div>
+
         <div className="container">
           <HomeGalery />
         </div>
